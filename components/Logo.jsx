f61d05e5b0857208820f10702687fe8efd229e0e
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-06-07 01:11:33 +0500

    intro completely white

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,6 +3,7 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
 import { useStateStore } from "@/stores/state";
+import { MeshBasicMaterial } from "three";
 
 export function Intro(props) {
   const group = useRef();
@@ -26,18 +27,19 @@ export function Intro(props) {
       setIntroDone(!actions["Shape1Action.002"].isRunning());
     }
   })
+  const white_mat = new MeshBasicMaterial({color: 'white'});
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <mesh name="Shape1" geometry={nodes.Shape1.geometry} material={materials['15 - Default']} position={[-10, 0, 0]} rotation={[Math.PI / 2, 0, -Math.PI]} scale={0} />
-        <mesh name="Curve002" geometry={nodes.Curve002.geometry} material={materials.SVGMat} position={[-0.362, 0, -0.009]} />
-        <mesh name="Curve003" geometry={nodes.Curve003.geometry} material={materials.SVGMat} position={[-0.257, 0, -0.004]} />
-        <mesh name="Curve005" geometry={nodes.Curve005.geometry} material={materials.SVGMat} position={[-0.045, 0, -0.002]} />
-        <mesh name="Curve006" geometry={nodes.Curve006.geometry} material={materials.SVGMat} position={[0.058, 0, -0.003]} />
-        <mesh name="Curve008" geometry={nodes.Curve008.geometry} material={materials.SVGMat} position={[0.252, 0, -0.008]} />
-        <mesh name="Curve009" geometry={nodes.Curve009.geometry} material={materials.SVGMat} position={[0.355, 0, -0.003]} />
-        <mesh name="Curve010" geometry={nodes.Curve010.geometry} material={materials.SVGMat} position={[0.151, 0, -0.002]} />
-        <mesh name="Curve012" geometry={nodes.Curve012.geometry} material={materials.SVGMat} position={[-0.15, 0, 0.001]} />
+        <mesh name="Shape1" geometry={nodes.Shape1.geometry} material={white_mat} position={[-10, 0, 0]} rotation={[Math.PI / 2, 0, -Math.PI]} scale={0} />
+        <mesh name="Curve002" geometry={nodes.Curve002.geometry} material={white_mat} position={[-0.362, 0, -0.009]} />
+        <mesh name="Curve003" geometry={nodes.Curve003.geometry} material={white_mat} position={[-0.257, 0, -0.004]} />
+        <mesh name="Curve005" geometry={nodes.Curve005.geometry} material={white_mat} position={[-0.045, 0, -0.002]} />
+        <mesh name="Curve006" geometry={nodes.Curve006.geometry} material={white_mat} position={[0.058, 0, -0.003]} />
+        <mesh name="Curve008" geometry={nodes.Curve008.geometry} material={white_mat} position={[0.252, 0, -0.008]} />
+        <mesh name="Curve009" geometry={nodes.Curve009.geometry} material={white_mat} position={[0.355, 0, -0.003]} />
+        <mesh name="Curve010" geometry={nodes.Curve010.geometry} material={white_mat} position={[0.151, 0, -0.002]} />
+        <mesh name="Curve012" geometry={nodes.Curve012.geometry} material={white_mat} position={[-0.15, 0, 0.001]} />
       </group>
     </group>
   );
