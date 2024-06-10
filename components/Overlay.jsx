c902b697b116e0641d5f1ae0502a9331d94d860e
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-06-11 01:51:21 +0500

    outline action buttons

--- components/Overlay.jsx
+++ components/Overlay.jsx
@@ -7,7 +7,7 @@ const Overlay = () => {
   const { setSection, section } = useStateStore();
 
   return (
-    <div className="w-screen h-screen absolute top-0 left-0 z-50 pointer-events-none font-sans text-4xl lg:text-7xl font-bold transition-colors">
+    <div className="w-screen h-screen absolute top-0 left-0 z-50 pointer-events-none font-sans text-5xl lg:text-7xl font-bold transition-colors">
       {/* <Image src={"/images/logo_name.jpg"} width={150} height={100} alt="logo" className="absolute top-10 left-10" /> */}
       <div className="w-full h-full relative">
         {section === "home" ? (
@@ -20,12 +20,20 @@ const Overlay = () => {
               className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-20 lg:w-40"
             />
             <div className="cursor-pointer">
-              <div className="absolute top-1/4 left-1/4 pointer-events-auto glow black text-white" onClick={() => setSection("black")}>
-                MEN
+              <div
+                className="absolute top-1/4 left-1/2 -translate-x-1/2 lg:left-1/4 pointer-events-auto text-white flex items-center justify-center flex-col"
+                onClick={() => setSection("black")}
+              >
+                <div className="outlineT white">MEN</div>
                 <div className="text-lg pointer-events-auto lg:text-2xl">← explore</div>
               </div>
-              <div className="absolute bottom-1/4 right-1/4 pointer-events-auto glow white text-black" onClick={() => setSection("white")}>
-                WOMEN
+
+              <div
+                className="absolute bottom-1/4 left-1/2 lg:right-1/4 -translate-x-1/2 lg:translate-x-1/2 pointer-events-auto  text-black flex items-center justify-center flex-col"
+                onClick={() => setSection("white")}
+              >
+                <div className="outlineT black">WOMEN</div>
+
                 <div className="text-lg pointer-events-auto lg:text-2xl text-right">explore →</div>
               </div>
             </div>
